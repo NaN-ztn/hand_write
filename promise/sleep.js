778f1c0760cb787df53d113aa5d117796cebbdc8
nannn <2337286751@qq.com>
2022-05-11 00:20:50 +0800

    修改sleep单测

--- promise/sleep.js
+++ promise/sleep.js
@@ -1,5 +1,7 @@
 export function sleep(duration) {
   return new Promise((resolve) => {
     setTimeout(resolve, duration, `sleep ${duration}`);
-  })
+  });
 }
+
+// 可用生成器实现
--- promise/sleep.spec.js
+++ promise/sleep.spec.js
@@ -1,3 +1,4 @@
+import { trimEnd } from 'lodash';
 import { sleep } from './sleep';
 
 test('sleep', async () => {
@@ -10,6 +11,14 @@ test('sleep', async () => {
   // 方法二
   // return expect(sleep(1000)).resolves.toBe('sleep 1000');
   // 方法三
+
+  console.time('sleep');
+  // sleep(500).then(() => {
+  //   console.timeEnd('sleep');
+  // });
+  await sleep(500);
+  console.timeEnd('sleep');
+  
   const callback = jest.fn();
   setTimeout(callback, 1000);
   await sleep(1000);
