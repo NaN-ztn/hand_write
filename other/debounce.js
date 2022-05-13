ba5fe82194f15338430c360bf16bbe79207b45a8
nannn <2337286751@qq.com>
2022-05-14 03:15:11 +0800

    完善debounce和throttle

--- other/debounce.js
+++ other/debounce.js
@@ -2,32 +2,31 @@
 // 场景：搜索框防抖，浏览器滚动，鼠标移动事件 resize事件等
 // 在不触发事件后一段时间内执行（后沿）
 // 与节流最大区别在于节流时每隔一定时间至少执行一次
-function debounce(fn, wait) {
+export function debounce(fn, wait) {
   let timer = null;
-  return function () {
-    let self = this;
-    let args = arguments;
-    if (timer) {
-      clearTimeout(timer);
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
       timer = null;
-    }
-    timer = setTimeout(() => fn.apply(self, args), wait);
+      fn(...args);
+    }, wait);
   };
 }
 
-function debounce(fn, delay = 500) {
+function _debounce(fn, delay = 500) {
   let timer = null;
   return (...args) => {
     clearTimeout(timer);
     return new Promise((resolve) => {
       timer = setTimeout(() => {
+        timer = null;
         resolve(fn(...args));
       }, delay);
     });
   };
 }
 
- _.debounce();
+// _.debounce();
 // lodash 中每次调用 的 debounced 函数修改 lastCallTime 变量，
 // 在定时器到期时进行判断，若不执行，则重置定时器，用重新计算的还需等待时间作为定时器延时时间
 // 每次调用 的 deounced 函数时，都会先走 leadingEdge 分支，以判断是否时前沿调用
--- other/throttle.js
+++ other/throttle.js
@@ -1,18 +1,29 @@
 // 场景：无限滚动，高频点赞事件
-function throttle(fn, wait) {
+// 时间戳
+export function throttle(fn, wait) {
   let cur = Date.now();
-  return function () {
-    let self = this;
-    let args = arguments;
+  return function (...args) {
     let now = Date.now();
-    if (now - cur >= 0) {
+    if (now - cur >= wait) {
       cur = Date.now();
-      return fn.apply(self, args);
+      return fn(...args);
     }
   };
 }
 
-function throttle(fn, delay = 100) {
+// 定时器
+export function __throttle(fn, wait) {
+  let timer = null;
+  return function (...args) {
+    !timer &&
+      (timer = setTimeout(() => {
+        timer = null;
+        fn(...args);
+      }, wait));
+  };
+}
+
+function _throttle(fn, delay = 100) {
   let timer = null;
   return (...args) => {
     if (timer) {
@@ -35,3 +46,7 @@ function throttle(fn, delay = 100) {
 // 函数重新计算并在屏幕上渲染元素，并且希望保证平滑的更改或动画时
 
 // 涉及到 ajax 请求/移出添加类时一般使用节流或防抖
+
+// lodash 中 throttle 为 debounce 的变式，将 leading 和 trailing 设置为 true
+// 前沿执行后，会将参数置为 undefined 就无法执行后沿，除非在前沿执行完成后，但本次周期尚未完成时再次触发事件，给参数赋值，就能执行后沿。
+// 中间无论触发多少次，都只有前沿和后沿执行
